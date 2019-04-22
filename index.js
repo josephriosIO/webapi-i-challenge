aa9f2f2dc8f340b53fb940e870998dffb800cd85
josephriosio <blankworldwide@gmail.com>
2019-04-22 12:53:36 -0700

    setup server with tthe get all users request working

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+const express = require("express");
+
+//local imports
+const db = require("./data/db");
+
+//being able to use express as a server
+const server = express();
+
+//make express be able to read json format
+server.use(express.json());
+
+//GET request
+server.get("/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.json({ error: "The users infomation could not be retrieved" });
+    });
+});
+
+server.listen(5000, () => {
+  console.log("\n *** BACKEND PROJECT 1 RUNNING ON PORT 5000 *** \n");
+});
